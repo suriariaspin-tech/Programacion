25aa978926d0b6d49d4dd782ff311b6435737cf6
Suri Arias <249509373+suriariaspin-tech@users.noreply.github.com>
2026-04-15 19:39:36 +0000

    commit funciones

---
+++ funciones.c
@@ -0,0 +1,14 @@
+void registrosucursal(char lista, char name, int iterador){
+    lista[iterador] = name;
+}
+
+void registroproductos(char lista2, char name2, int iterador2){
+    lista2[iterador2] = name2;
+
+}
+
+int main(){
+    char milista[3]
+    registrosucursal(milista, "suscurla1", )
+}
+
